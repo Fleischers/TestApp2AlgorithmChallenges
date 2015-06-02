89d2555a22b2cad8d61871305e811a3dbd8059ec
Glib Fleischer Ischenko <Fleischer458@gmail.com>
2015-06-02 12:37:48 +0300

    Heroku pusblish

--- index.js
+++ index.js
@@ -8,3 +8,16 @@ exec('node challenge1.js', function (error, stdout, stderr) {
 
     });
 });
+
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 3000));
+
+app.get('/', function(request, response) {
+  response.send("Input");
+});
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running on port:" + app.get('port'))
+})
